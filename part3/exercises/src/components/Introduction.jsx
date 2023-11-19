63c4d15a40c27397fcec0efc3d489f8d9bb0aced
Kevin Wiegman <kevin@Kevins-MacBook-Air.local>
2023-11-19 12:51:54 -0600

    exercise complete

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
-         <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+         <h2>3 Fun Facts About Painting</h2>
+         <ol className = "funFacts">
+            <li>Artists Used to Store Their Paint in Animal Bladders until the Paint Tube was invented in 1841.</li>
+            <li>It takes 570 Gallons of Paint to Repaint The White House.</li>
+            <li>Early Cave paintings  were drawn with red or yellow ochre, hematite, manganese oxide, and charcoal may have been made by early Homo sapiens as long as 30,000 years ago.</li>
          </ol>
       </div>
    );
